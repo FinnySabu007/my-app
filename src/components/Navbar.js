4ee81df90b8dd9e8796c78e32cffa076ba0836f4
FinnySabu007 <finnysabu007@gmail.com>
2023-07-14 16:21:15 +0530

    change2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,18 +7,18 @@ export default function Navbar(props) {
     // <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} `}>
     <nav className={`navbar navbar-expand-lg navbar-light bg-light`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">{props.title}</a>
+        <a className="navbar-brand" href="#">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
-            </li>
+            </li> */}
           </ul>
           <div className="btn-group mx-4" role="group" aria-label="Basic mixed styles example">
   <button type="button" onClick={() => props.toggleMode("#343a40","white","dark")} style={{backgroundColor:'black',color:'white'}} className="btn">black</button>
